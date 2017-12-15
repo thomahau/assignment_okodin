97303ecab4d910ec95dd9c3e6331cccd8292215e
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2017-12-15 17:04:18 +0100

    views index with correct ordering

--- controllers/views.js
+++ controllers/views.js
@@ -3,11 +3,102 @@ const models = require('./../models');
 
 const router = express.Router();
 const sequelize = models.sequelize;
-const User = models.User;
 const Profile = models.Profile;
-// const Location = models.Location;
+const Location = models.Location;
 const View = models.View;
 
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+  let viewees = [];
+  let viewers = [];
+
+  sequelize
+    .transaction(t => {
+      return View.findAll({
+        where: {
+          viewerId: req.session.currentUser.id
+        },
+        order: [['updatedAt', 'DESC']],
+        transaction: t
+      })
+        .then(views => {
+          const vieweeIds = views.map(view => view.vieweeId);
+          views.forEach(view => {
+            viewees.push({
+              vieweeId: view.vieweeId,
+              updatedAt: Date.parse(view.updatedAt)
+            });
+          });
+
+          return Profile.findAll({
+            where: {
+              userId: vieweeIds
+            },
+            include: [{ all: true }],
+            transaction: t
+          });
+        })
+        .then(profiles => {
+          profiles.forEach(profile => {
+            viewees.forEach(viewee => {
+              if (viewee.vieweeId === profile.userId) {
+                profile.viewUpdatedAt = viewee.updatedAt;
+              }
+            });
+          });
+
+          viewees = profiles.sort(function(a, b) {
+            return b.viewUpdatedAt - a.viewUpdatedAt;
+          });
+
+          return View.findAll({
+            where: {
+              vieweeId: req.session.currentUser.id
+            },
+            order: [['updatedAt', 'DESC']],
+            transaction: t
+          });
+        })
+        .then(views => {
+          const viewerIds = views.map(view => view.viewerId);
+          views.forEach(view => {
+            viewers.push({
+              viewerId: view.viewerId,
+              updatedAt: Date.parse(view.updatedAt)
+            });
+          });
+
+          return Profile.findAll({
+            where: {
+              userId: viewerIds
+            },
+            include: [{ all: true }],
+            transaction: t
+          });
+        })
+        .then(profiles => {
+          profiles.forEach(profile => {
+            viewers.forEach(viewer => {
+              if (viewer.viewerId === profile.userId) {
+                profile.viewUpdatedAt = viewer.updatedAt;
+              }
+            });
+          });
+
+          viewers = profiles.sort(function(a, b) {
+            return b.viewUpdatedAt - a.viewUpdatedAt;
+          });
+
+          res.render('views/index', { viewees, viewers });
+        });
+    })
+    .catch(e => {
+      if (e.errors) {
+        e.errors.forEach(err => req.flash('error', err.message));
+        res.redirect('back');
+      } else {
+        res.status(500).send(e.stack);
+      }
+    });
+});
 
 module.exports = router;
--- migrations/20171215091714-create-view.js
+++ migrations/20171215091714-create-view.js
@@ -1,30 +1,34 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Views', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      viewerId: {
-        type: Sequelize.INTEGER
-      },
-      vieweeId: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.fn('NOW')
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.fn('NOW')
-      }
-    });
+    return queryInterface
+      .createTable('Views', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        viewerId: {
+          type: Sequelize.INTEGER
+        },
+        vieweeId: {
+          type: Sequelize.INTEGER
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.fn('NOW')
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.fn('NOW')
+        }
+      })
+      .then(() => {
+        return queryInterface.addIndex('Views', ['updatedAt']);
+      });
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('Views');
